fd2fc45482ed369b3502cdc8bc84f9d6bd3e4afa
joud <joudsba@gmail.com>
2025-12-01 11:35:07 +0300

    Initial Commit

---
+++ awn app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  awn app
+//
+//  Created by Joud Almashgari on 01/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ awn app/awn_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  awn_appApp.swift
+//  awn app
+//
+//  Created by Joud Almashgari on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct awn_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
