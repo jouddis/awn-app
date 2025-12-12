2df939219ea62fd5836cfcbb4436402195f1437c
joud <joudsba@gmail.com>
2025-12-12 13:59:34 +0300

    watchapp

---
+++ AwnWatch Watch App/AwnWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwnWatchApp.swift
+//  AwnWatch Watch App
+//
+//  Created by Joud Almashgari on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct AwnWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwnWatch Watch App/Services/WatchViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  awn app
+//
+//  Created by Joud Almashgari on 11/12/2025.
+//
+
